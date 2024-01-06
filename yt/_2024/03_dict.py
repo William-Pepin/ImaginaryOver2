96d8c74eec4ef4f13055bf093e594005c75889c2
William Pepin <wpepin00@gmail.com>
2024-01-06 14:16:22 -0500

    video dict 3 completion

--- yt/_2024/03_dict.py
+++ yt/_2024/03_dict.py
@@ -1,6 +1,10 @@
 from manim import *
 from yt.config.config import cf
 from yt.helper.title_helper import TitleHelper
+from yt.helper.code_helper import CodeHelper
+from yt.core.dict_of_text import DictOfText
+from yt.core.dict_of_mobject import DictOfMobject
+from yt.helper.fade_out_helper import fade_out_all
 
 
 config.background_color = "#1F2430"
@@ -19,123 +23,77 @@ class Intro(Scene):
 
 class Scene_1(Scene):
     def construct(self):
-        keys_strings = ["id", "name", "surname", "category", "logged_in"]
-        data_strings = ["935", "John", "Smith", "Employee", "False"]
+        keys = ["id", "name", "surname", "category", "logged_in"]
+        datas = ["935", '"John"', '"Smith"', '"Employee"', "False"]
+        datas_for_dict = ["935", 'John', 'Smith', 'Employee', "False"]
+        code = \
+            """
+user = {"id": 935,
+    "name": "John",
+    "surname": "Smith",
+    "category": "Employee",
+    "logged_in": False}
+print(user["name"])
+"""
+        code_helper = CodeHelper(self)
+        code_layout = code_helper.generate_code_layout(code)
 
-        keys = VGroup(
-            *[
-                Text(text, font=default_font, font_size=48, color=cf.BLUE)
-                for text in keys_strings
-            ]
-        )
+        code_helper.show_code_block(code_layout)
+        code_helper.write_code(code_layout)
 
-        data = VGroup(
-            *[
-                Text(text, font=default_font, font_size=24, color=cf.BLUE)
-                for text in data_strings
-            ]
-        )
+        self.wait(cf.RATE_SLOW)
 
-        data.arrange(RIGHT)
-        names.arrange(LEFT)
+        self.play(code_helper.highlight_in_code(
+            code_layout, "{"), code_helper.highlight_in_code(code_layout, "}"))
 
-        identificators.shift(UP * 6)
-        names.shift(UP * 4)
+        self.play(*[code_helper.highlight_in_code(code_layout, f'"{key}"')
+                    for key in keys])
+        self.play(*[code_helper.highlight_in_code(code_layout, data)
+                  for data in datas])
 
-        brackets = VGroup(
-            Text("[", font=default_font, font_size=64, color=cf.YELLOW).next_to(
-                identificators, LEFT
-            ),
-            Text(f"]", font=default_font, font_size=64, color=cf.YELLOW).next_to(
-                identificators, RIGHT
-            ),
-        )
+        self.wait(cf.RATE_SLOW)
 
-        brackets_2 = VGroup(
-            Text(
-                "[",
-                font=default_font,
-                font_size=64,
-                color=cf.YELLOW,
-            ).next_to(names, LEFT),
-            Text(
-                f"]",
-                font=default_font,
-                font_size=64,
-                color=cf.YELLOW,
-            ).next_to(names, RIGHT),
-        )
+        code_layout.generate_target()
+        code_layout.target.shift(DOWN*5.5)
+        self.play(MoveToTarget(code_layout))
+
+        dict_of_text = DictOfText(keys, datas_for_dict, 42, 42, 36)
+
         self.play(
-            FadeIn(identificators), FadeIn(
-                brackets), FadeIn(names), FadeIn(brackets_2)
-        )
-        self.wait(0.5)
+            FadeIn(dict_of_text.table))
 
-        identificators_end = VGroup(
-            *[
-                Text(
-                    text, font=default_font, font_size=64, color=cf.BLUE, line_spacing=4
-                )
-                for text in reversed(raw_identificators)
-            ]
-        )
+        self.play(code_helper.highlight_in_code(
+            code_layout, 'print(user["name"])'))
 
-        names_end = VGroup(
-            *[
-                Text(
-                    text,
-                    font=default_font,
-                    font_size=64,
-                    color=cf.BLUE,
-                    line_spacing=4,
-                )
-                for text in reversed(raw_names)
-            ]
-        )
+        self.play(code_helper.highlight(dict_of_text.get_key(1)))
+        self.play(code_helper.highlight(dict_of_text.get_value(1)))
 
-        double_dots = VGroup(
-            *[
-                Text(
-                    ":",
-                    font=default_font,
-                    font_size=64,
-                    color=cf.ORANGE,
-                    line_spacing=4,
-                )
-                for _ in range(5)
-            ]
-        )
+        self.wait(cf.RATE_END)
 
-        identificators_end.arrange_in_grid(5, 1).shift(LEFT * 2.5)
-        double_dots.arrange_in_grid(5, 1, buff=0.462).align_to(
-            identificators_end
-        ).shift(LEFT * 1.5)
-        names_end.arrange_in_grid(5, 1, col_alignments="l").shift(RIGHT * 1.5).align_to(
-            identificators_end, DOWN
-        )
-
-        for i in reversed(range(1, 5)):
-            self.play(
-                Transform(identificators[i], identificators_end[4 - i]),
-                Transform(names[i], names_end[4 - i]),
-                FadeIn(double_dots[4 - i], run_time=1.5),
-            )
-        self.play(
-            FadeOut(brackets, brackets_2),
-            Transform(identificators[0], identificators_end[4]),
-            Transform(names[0], names_end[4]),
-            FadeIn(double_dots[4], run_time=1.2),
-        )
-        self.wait()
-        self.play(
-            FadeOut(identificators, run_time=0.5),
-            FadeOut(double_dots, run_time=0.5),
-            FadeOut(names, run_time=0.5),
-        )
+        fade_out_all(self)
 
 
 class Scene_2(Scene):
     def construct(self):
+        code = """
+shapes = [Square(), Circle(),
+          Square(), Triangle(), Circle()]
+freq = {}
+for shape in shapes:
+    freq[shape] = shapes.count(shape)
+"""
+        code_helper = CodeHelper(self)
+        code_layout = code_helper.generate_code_layout(code)
+
+        code_helper.show_code_block(code_layout)
+        code_helper.write_code(code_layout)
+
+        code_layout.generate_target()
+        code_layout.target.shift(DOWN*4)
+        self.wait(cf.RATE_SLOW)
+
+        self.play(MoveToTarget(code_layout))
+
         base_shapes = VGroup(
             Square(color=cf.GREY, side_length=0.82),
             Circle(color=cf.BLUE, radius=0.4),
@@ -154,56 +112,35 @@ class Scene_2(Scene):
             ),
         )
 
-        self.play(FadeIn(base_shapes, brackets))
-        self.wait(0.5)
-
-        shape_end = (
-            VGroup(
-                Square(color=cf.GREY, side_length=0.82),
-                Circle(color=cf.BLUE, radius=0.4),
-                Triangle(color=cf.PURPLE).scale(0.45),
-            )
-            .arrange(DOWN)
-            .shift(LEFT * 0.5)
+        keys = VGroup(Square(color=cf.GREY, side_length=0.82),
+                      Circle(color=cf.BLUE, radius=0.4), Triangle(color=cf.PURPLE).scale(0.45))
+        values = VGroup(
+            *[DecimalNumber(0, font_size=64, num_decimal_places=0) for _ in range(3)]
         )
+        m_dict = DictOfMobject(
+            keys, values, [Text(header, font_size=48, color=cf.ORANGE) for header in ("Keys", "Values")])
 
-        double_dots = VGroup(
-            *[
-                Text(":", font=cf.DEFAULT_FONT, font_size=64, color=cf.ORANGE)
-                for _ in range(3)
-            ]
-        )
+        self.play(FadeIn(base_shapes, brackets,
+                  m_dict.get_table_without_elements(), m_dict.table.get_columns()[1]))
+        self.wait(cf.RATE_SLOW)
 
-        count_state = VGroup(
-            *[DecimalNumber(1, font_size=64, num_decimal_places=0) for _ in range(3)]
-        )
+        self._count_shape(
+            base_shapes[0], m_dict.get_key(0), m_dict.get_value(0))
+        self._count_shape(
+            base_shapes[1], m_dict.get_key(1), m_dict.get_value(1))
+        self._count_shape(
+            base_shapes[2], m_dict.get_key(0), m_dict.get_value(0))
+        self._count_shape(
+            base_shapes[3], m_dict.get_key(2), m_dict.get_value(2))
+        self._count_shape(
+            base_shapes[4], m_dict.get_key(1), m_dict.get_value(1))
+        self.play(FadeOut(brackets))
+        self.wait(cf.RATE_END)
+        fade_out_all(self)
 
+    def _count_shape(self, shape, dict_key, dict_value):
         self.play(
-            Transform(base_shapes[0], shape_end[0]),
-            FadeIn(double_dots[0].next_to(shape_end[0])),
-            FadeIn(count_state[0].next_to(double_dots[0], RIGHT * 2.5)),
+            Transform(shape, dict_key),
+            ChangeDecimalToValue(dict_value, dict_value.number + 1),
         )
-        self.play(
-            Transform(base_shapes[1], shape_end[1]),
-            FadeIn(double_dots[1].next_to(shape_end[1])),
-            FadeIn(count_state[1].next_to(double_dots[1], RIGHT * 2.5)),
-        )
-        self.play(
-            Transform(base_shapes[2], shape_end[0]),
-            ChangeDecimalToValue(count_state[0], 2),
-        )
-
-        self.play(
-            Transform(base_shapes[3], shape_end[2]),
-            FadeIn(double_dots[2].next_to(shape_end[2])),
-            FadeIn(count_state[2].next_to(double_dots[2], RIGHT * 2.5)),
-        )
-
-        self.play(
-            Transform(base_shapes[4], shape_end[1]),
-            ChangeDecimalToValue(count_state[1], 2),
-            FadeOut(brackets),
-        )
-
-        self.wait()
-        self.play(FadeOut(count_state, double_dots, base_shapes))
+        self.wait(cf.RATE_MEDIUM)
